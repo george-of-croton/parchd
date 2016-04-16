f5bee0491650143ba383593bdd53e13043bceb01
George Armstrong <email": null>
2016-04-16 20:03:28 +1200

    maKE MORE CHANGES

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,38 @@
-console.log(document.getElementById("create"))
- var createButton = document.getElementById("create")
+var request = require('superagent')
+var createButton = document.getElementById("creator")
+var submitButton = document.getElementById('submitBtn')
+var formData = new FormData();
 
- createButton.addEventListener("click", visitCreatePage)
+if(document.getElementById("water")){ //all /water logic goes here
+  createButton.addEventListener("click", visitCreatePage)
+}
 
- function visitCreatePage() {
-  window.location = "/create"
+
+if(document.getElementById("create")){ // all /create logic goes here
+  submitButton.addEventListener("click", submitCreateForm)
+}
+
+
+ function visitCreatePage() { //
+  // window.location = "/create"
+ }
+
+ function submitCreateForm() {
+  console.log("hello")
+  var form = {
+    name: document.getElementById("name_form").value
+  }
+  console.log(form)
+  request
+    .post('/create')
+    .send(form)
+    // .set('Accept', 'application/json')
+    .end(function(err, res){
+      if(err){
+        console.log(err)
+      }
+      console.log("we are cooking with gas, admiral!")
+    })
  }
 
 
